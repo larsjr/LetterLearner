f15d03edf8c6bb50bf524a4fa5cdced6b441109d
Lars Jahr Røine <larsroine@icgi.no>
2020-03-13 16:06:24 +0100

    Created view model with binding to MainPage

---
+++ LetterLearner/ViewModels/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace LetterLearner.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private string _helloWorld;
+
+        public string HelloWorld
+        {
+            get { return _helloWorld; }
+            set { Set(() => HelloWorld, ref _helloWorld, value); }
+        }
+
+        public MainViewModel()
+        {
+            HelloWorld = IsInDesignMode
+                ? "Runs in design mode"
+                : "Runs in runtime mode";
+
+        }
+    }
+}
---
+++ LetterLearner/ViewModels/ViewModelLocator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonServiceLocator;
+using GalaSoft.MvvmLight.Ioc;
+
+namespace LetterLearner.ViewModels
+{
+    public class ViewModelLocator
+    {
+        public MainViewModel Main => ServiceLocator.Current.GetInstance<MainViewModel>();
+
+        static ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+    }
+}
